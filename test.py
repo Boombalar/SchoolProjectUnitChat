9314a4647f47b5fc848bfc9934614fb353a5ff9d
Boombalar <mathias_1337@hotmail.com>
2013-01-22 13:35:59 +0100

    En lille hilsen fra mig til Jeppe ;)

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-variable = "kage"
+import time
+variable  = "Jeppe"
+variable2 = "er"
+variable3 = "grim"
+variable4 = "FAIL!"
 print (variable)
-if variable == "kage":
-    print ("lol")
-else:
-    print ("FAIL!")
+time.sleep(1)
+print (variable2)
+time.sleep(1)
+if variable2 == (variable2):
+    time.sleep(1)
+    print (variable3)
+    time.sleep(1)
+else: print (variable4)
