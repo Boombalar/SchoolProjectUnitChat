acc42cb962eea8fe4235926a2928ef72799235f7
Boombalar <mathias_1337@hotmail.com>
2013-01-28 12:15:48 +0100

    Pythagoras fixed v1.0
    ex.
    "Hvilken side skal du finde? "c"
    "Hvor lang er side a? "3"
    "Hvor lang er side b?" "4"
    "Resultat" AKA side c = "5"

--- Pythagoras.py
+++ Pythagoras.py
@@ -7,18 +7,18 @@ if side == "a":
     c = input ("Hvor lang er siden c? ")
     btal = float(b)
     ctal = float(c)
-    resultat = (ctal ** 2 - btal ** 2)
+    resultat = (math.sqrt(ctal ** 2 - btal ** 2))
 elif side == "b":
     a = input ("Hvor lang er siden a? ")
     c = input ("Hvor lang er siden c? ")
     atal = float(a)
     ctal = float(c)
-    resultat = (ctal ** 2 - atal ** 2)
+    resultat = (math.sqrt(ctal ** 2 - atal ** 2))
 elif side == "c":
     a = input ("Hvor lang er siden a? ")
     b = input ("Hvor lang er siden b? ")
     atal = float(a)
     btal = float(b)
-    resultat = (atal ** 2 + btal ** 2)
+    resultat = (math.sqrt(atal ** 2 + btal ** 2))
 
 print ("Resultatet er: ", resultat)
