879f767ea7d2037d5bbf5e30fdfdd9f19b0dd41a
Lasse Dissing Hansen <volpes@gmx.com>
2013-01-28 12:04:49 +0100

    Fixed String -> Float conversion

--- Pythagoras.py
+++ Pythagoras.py
@@ -5,18 +5,20 @@ side = input ("Hvilken side skal du finde? ")
 if side == "a":
     b = input ("Hvor lang er siden b? ")
     c = input ("Hvor lang er siden c? ")
+    btal = float(b)
+    ctal = float(c)
     resultat = (ctal ** 2 - btal ** 2)
 elif side == "b":
     a = input ("Hvor lang er siden a? ")
     c = input ("Hvor lang er siden c? ")
+    atal = float(a)
+    ctal = float(c)
     resultat = (ctal ** 2 - atal ** 2)
 elif side == "c":
     a = input ("Hvor lang er siden a? ")
     b = input ("Hvor lang er siden b? ")
+    atal = float(a)
+    btal = float(b)
     resultat = (atal ** 2 + btal ** 2)
-
-atal = float(a)
-btal = float(b)
-ctal = float(c)
 
 print ("Resultatet er: ", resultat)
