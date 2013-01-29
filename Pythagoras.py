e4dc2ab00909babc471f211be40bebda80444c86
Boombalar <mathias_1337@hotmail.com>
2013-01-29 09:29:48 +0100

    Pythagoras v1.1

--- Pythagoras.py
+++ Pythagoras.py
@@ -2,23 +2,42 @@ import math
 
 side = input ("Hvilken side skal du finde? ")
 
-if side == "a":
-    b = input ("Hvor lang er siden b? ")
-    c = input ("Hvor lang er siden c? ")
-    btal = float(b)
-    ctal = float(c)
-    resultat = (math.sqrt(ctal ** 2 - btal ** 2))
-elif side == "b":
-    a = input ("Hvor lang er siden a? ")
-    c = input ("Hvor lang er siden c? ")
-    atal = float(a)
-    ctal = float(c)
-    resultat = (math.sqrt(ctal ** 2 - atal ** 2))
-elif side == "c":
-    a = input ("Hvor lang er siden a? ")
-    b = input ("Hvor lang er siden b? ")
-    atal = float(a)
-    btal = float(b)
-    resultat = (math.sqrt(atal ** 2 + btal ** 2))
+running = True
 
-print ("Resultatet er: ", resultat)
+while running == True:
+    resultat = 0
+    flag = True
+    if side == "a":
+        b = input ("Hvor lang er siden b? ")
+        c = input ("Hvor lang er siden c? ")
+        btal = float(b)
+        ctal = float(c)
+        if float(c) < float(b):
+            print ("Dette er ikke en ret vinkel")
+            flag = False
+        elif float(c) > float(b):
+            resultat = (math.sqrt(ctal ** 2 - btal ** 2))
+    elif side == "b":
+        a = input ("Hvor lang er siden a? ")
+        c = input ("Hvor lang er siden c? ")
+        atal = float(a)
+        ctal = float(c)
+        if float(c) < float(a):
+                print ("Dette er ikke en ret vinkel")
+                flag = False
+        elif float(c) > float(a):
+            resultat = (math.sqrt(ctal ** 2 - atal ** 2))
+    elif side == "c":
+        a = input ("Hvor lang er siden a? ")
+        b = input ("Hvor lang er siden b? ")
+        atal = float(a)
+        btal = float(b)
+        resultat = (math.sqrt(atal ** 2 + btal ** 2))
+    if flag == True:
+        print ("Resultatet er: ", resultat)
+
+    fortsæt = input("Vil du fortsæte")
+    if fortsæt == "Ja":
+        running == True
+    else:
+        running == False
