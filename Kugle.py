250430efa7e60c50374d08aa262dd5da0c41f703
Lasse Dissing Hansen <volpes@gmx.com>
2013-01-30 13:22:46 +0100

    Omskrev @Snegl16 kugle.py til det format som programmet skal laves i

--- Kugle.py
+++ Kugle.py
@@ -1,23 +1,90 @@
 import math
+LANG = 0
+def ball():
+    question = ("Hvad vil du vide: ", "What do you want to know: ")
+    knowledge = input(question[LANG])
+    knowledge = knowledge.lower()
+    if knowledge == "area" or knowledge == "areal":
+        area()
+    elif knowledge == "radius":
+        radius()
+    elif knowledge == "rumfang" or knowledge == "volume":
+        volume()
+        
+        
 
-road = input ("Hvad vil du finde?   ")
-if road == ("Overfladeareal"):
-    Radius = input (" Hvad er din radius?  ")
-    Radius = float (Radius)
-    A = 4 * math.pi * Radius**2
-    print (A)
-elif road == ("Rumfang"):
-    Rumfang = input ("Hvad er din radius?   ")
-    Rumfang = float (Radius)
-    V = (3/4) * math.pi * Radius**3
-    print (V)
-elif road == ("Radius ved hjælp af overfladeareal"):
-    Areal= input ("Hvad er dit Areal?      ")
-    Areal= float (Areal)
-    ra = (Areal/(4*math.pi))**(1/2)
-    print (ra)
-elif road == ("Radius ved hjælp af rumfang"):
-    Rumfang= input ("Hvad er dit rumfang?  ")
-    Rumfang= float (Rumfang)
-    rv = (Rumfang *(3/(4*math.pi)))**(1/3)
-    print (rv)
+
+def area():
+    question = ("Hvad kender du allerede: ","What do you all ready know: ")
+    knowledge = input(question[LANG])
+    knowledge = knowledge.lower()
+    question2 = ("Hvad er " + knowledge+ "? ", "What is " + knowledge + "?")
+    knowledge2 = input(question2[LANG])
+    if knowledge == "radius":
+        resultat = areaFromRadius(knowledge2)
+    elif knowledge == "volume" or knowledge == "rumfang":
+        resultat = areaFromVolume(knowledge2)
+
+    print ("Resultatet er :", resultat)
+        
+
+def radius():
+    question = ("Hvad kender du allerede: ","What do you all ready know: ")
+    knowledge = input(question[LANG])
+    knowledge = knowledge.lower()
+    question2 = ("Hvad er " + knowledge + "? ", "What is " + knowledge + "?")
+    knowledge2 = input(question2[LANG])
+    if knowledge == "area" or knowledge == "areal":
+        resultat = radiusFromArea(knowledge2)
+    elif knowledge == "volume" or knowledge == "rumfang":
+        resultat = radiusFromVolume(knowledge2)
+
+    print ("Resultatet er :", resultat)
+    
+def volume():
+    question = ("Hvad kender du allerede: ","What do you all ready know: ")
+    knowledge = input(question[LANG])
+    knowledge = knowledge.lower()
+    question2 = ("Hvad er " + knowledge + "? ", "What is " + knowledge + "?")
+    knowledge2 = input(question2[LANG])
+    if knowledge == "radius":
+        resultat = volumeFromRadius(knowledge2)
+    elif knowledge == "area" or knowledge == "areal":
+        resultat = volumeFromArea(knowledge2)
+
+    print ("Resultatet er :", resultat) 
+    
+
+def radiusFromArea(area):
+    area = float (area)
+    radius = (Areal/(4*math.pi))**(1/2)
+    return radius
+
+def radiusFromVolume(rumfang):
+    rumfang = float (rumfang)
+    radius = (rumfang *(3/(4*math.pi)))**(1/3)
+    return radius
+    
+
+def areaFromRadius(radius):
+    radius = float (radius)
+    area = 4 * math.pi * radius**2
+    return area
+
+def areaFromVolume(rumfang):
+    rumfang = float (rumfang)
+    radius = radiusFromVolume(rumfang)
+    area = areaFromRadius(radius)
+    return area
+    
+
+def volumeFromRadius(radius):
+    radius = float (radius)
+    volume = (4/3) * math.pi * radius**3
+    return volume
+
+def volumeFromArea(area):
+    area = float(area)
+    radius = radiusFromArea(area)
+    volume = volumeFromRadius(radius)
+    return area
