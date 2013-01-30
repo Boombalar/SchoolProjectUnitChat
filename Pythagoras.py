cf80bcbbcff232e84f4af5eee07c93679bda9b00
Boombalar <mathias_1337@hotmail.com>
2013-01-30 13:10:32 +0100

    Pythagoras v.2.1.3
    Minor edits
    Lidt mellemrum, nye linjer etc hist og her.

--- Pythagoras.py
+++ Pythagoras.py
@@ -4,12 +4,12 @@ import sys
 running = True
 
 while running == True:
-    side = input ("which side do you want to find? ")
+    side = input ("which side do you want to find?\n")
     result = 0
     flag = True
     if side == "a" or side == "A":
-        bString = input ("How long is side b? ")
-        cString = input ("How long is side c? ")
+        bString = input ("How long is side b?\n")
+        cString = input ("How long is side c?\n")
         b = float(bString)
         c = float(cString)
         if (c) < (b):
@@ -19,8 +19,8 @@ while running == True:
             result = (math.sqrt(c ** 2 - b ** 2))
             
     elif side == "b" or side == "B":
-        aString = input ("How long is side a? ")
-        cString = input ("How long is side c? ")
+        aString = input ("How long is side a?\n")
+        cString = input ("How long is side c?\n")
         a = float(aString)
         c = float(cString)
         if (c) < (a):
@@ -30,17 +30,18 @@ while running == True:
             result = (math.sqrt(c ** 2 - a ** 2))
             
     elif side == "c" or side == "C":
-        aString = input ("How long is side a? ")
-        bString = input ("How long is side b? ")
+        aString = input ("How long is side a?\n")
+        bString = input ("How long is side b?\n")
         a = float(aString)
         b = float(bString)
         result = (math.sqrt(a ** 2 + b ** 2))
     if flag == True:
-        print ("The result is: ", result)
+
+        print ("The result is:\n",result)
+        
     print (" ")
     print ("Do you want to continue?")
-    print ("Yes for continue   No for exit")
-    keeprunning = input(" ")
+    keeprunning = input ("Yes for continue   No for exit\n")
     if keeprunning == "Yes" or keeprunning == "yes":
         running == True
     else:
