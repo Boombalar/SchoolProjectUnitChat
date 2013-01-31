ca3cee40502640f83fd6d1986b1684da7a303d51
Boombalar <mathias_1337@hotmail.com>
2013-01-31 12:25:13 +0100

    Pythagoras Funktion v1.1 BETA
    Lidt mellemrum hist og her, lidt ekstra brugervenlighed.

--- Pythagoras funktion.py
+++ Pythagoras funktion.py
@@ -1,55 +1,55 @@
 import math
 
-language = input ("Sprog/Language ")
+language = input ("Sprog/Language\n")
 if language == "English" or language == "english":
     LANG = 1
 elif language == "Dansk" or language == "dansk":
     LANG = 0
 
-side = ("Hvilken side vil du finde? ", "Which side do you want to find? ")
+side = ("Hvilken side vil du finde?\n", "Which side do you want to find?\n")
 side = input (side[LANG])
 
 
 if side == "a" or side == "A":
     def pythagorasA (pythagorasB, pythagorasC):
         if (pythagorasC) < (pythagorasB):
-            text4 = ("Denne trekant er ikke 90 grader!", "This is not a 90 degree triangle!")
-            input (text4[LANG])
+            ERROR = ("Denne trekant er ikke 90 grader!", "This is not a 90 degree triangle!")
+            input (ERROR[LANG])
         elif (pythagorasC) > (pythagorasB):
             pythagorasA = (math.sqrt(pythagorasC ** 2 - pythagorasB ** 2))
             return pythagorasA
 
-    text1 = ("Hvor lang er side b? ", "How long is side b? ")
-    text2 = ("Hvor lang er side c? ", "How long is side c? ")
-    text3 = ("Side a er: ", "Side a is: ")
+    sideB = ("Hvor lang er side b?\n", "How long is side b?\n")
+    sideC = ("Hvor lang er side c?\n", "How long is side c?\n")
+    sideA = ("Side a er:\n", "Side a is\n")
 
-    pythagorasB = input(text1[LANG])
-    pythagorasC = input(text2[LANG])
+    pythagorasB = input(sideB[LANG])
+    pythagorasC = input(sideC[LANG])
     pythagorasB = float(pythagorasB)
     pythagorasC = float(pythagorasC)
     resultat = pythagorasA (pythagorasB,pythagorasC)
-    print (text3[LANG], resultat)
+    print (sideA[LANG], resultat)
 
 
 elif side == "b" or side == "B":
     def pythagorasB (pythagorasA, pythagorasC):
         if (pythagorasC) < (pythagorasA):
-            text5 = ("Denne trekant er ikke 90 grader!", "This is not a 90 degree triangle!")
-            input (text5[LANG])
+            ERROR = ("Denne trekant er ikke 90 grader!", "This is not a 90 degree triangle!")
+            input (ERROR[LANG])
         elif (pythagorasC) > (pythagorasA):
             pythagorasB = (math.sqrt(pythagorasC ** 2 - pythagorasA ** 2))
             return pythagorasB
 
-    text6 = ("Hvor lang er side a? ", "How long is side a? ")
-    text7 = ("Hvor lang er side c? ", "How long is side c? ")
-    text8 = ("Side b er: ", "Side b is: ")
+    sideA = ("Hvor lang er side a?\n", "How long is side a?\n")
+    sideC = ("Hvor lang er side c?\n", "How long is side c?\n")
+    sideB = ("Side b er:\n", "Side b is:\n")
 
-    pythagorasA = input(text6[LANG])
-    pythagorasC = input(text7[LANG])
+    pythagorasA = input(sideA[LANG])
+    pythagorasC = input(sideC[LANG])
     pythagorasA = float(pythagorasA)
     pythagorasC = float(pythagorasC)
     resultat = pythagorasB (pythagorasA,pythagorasC)
-    print (text8[LANG], resultat)
+    print (sideB[LANG], resultat)
 
 
 elif side == "c" or side == "C":
@@ -57,15 +57,15 @@ elif side == "c" or side == "C":
         pythagorasC = (math.sqrt(pythagorasA ** 2 + pythagorasB ** 2))
         return pythagorasC
 
-    text9 = ("Hvor lang er side a? ", "How long is side a? ")
-    text10 = ("Hvor lang er side b? ", "How long is side b? ")
-    text11 = ("Side c er: ", "Side c is: ")
+    sideA = ("Hvor lang er side a?\n", "How long is side a?\n")
+    sideB = ("Hvor lang er side b?\n", "How long is side b?\n")
+    sideC = ("Side c er:\n", "Side c is:\n")
 
-    pythagorasA = input(text9[LANG])
-    pythagorasB = input(text10[LANG])
+    pythagorasA = input(sideA[LANG])
+    pythagorasB = input(sideB[LANG])
     pythagorasB = float(pythagorasB)
     pythagorasA = float(pythagorasA)
     resultat = pythagorasC (pythagorasB,pythagorasA)
-    print (text11[LANG], resultat)
+    print (sideC[LANG], resultat)
 
 
