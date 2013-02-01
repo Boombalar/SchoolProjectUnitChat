01d48c3118125fd14b7cfa374532fdfd55d7e55f
Boombalar <mathias_1337@hotmail.com>
2013-02-01 10:06:24 +0100

    Andengradsligninger v1.1 BETA
    NU MED TOPPUNKT JAAAAAAAA

--- Andengradsligninger.py
+++ Andengradsligninger.py
@@ -27,21 +27,35 @@ def andengradsligning():
     if (discriminant) < 0:
         ERROR = ("0 løsninger, diskriminatnen er negativ!", "0 solutions, the discriminant is negative!")
         print (ERROR[LANG])
+        return 0
+    
     elif (discriminant) == 0:
         discriminantText = ("Diskriminanten er\n", "The discriminant is\n",)
         print (discriminantText[LANG], discriminant)
         solution1 = ("1 løsning, diskriminaten er 0!", "1 solution, the discriminant is 0!")
         print (solution1[LANG])
-        singleSolution = "Løsningen er\n", "The solution is:\n"
-        print (singleSolution[LANG], (-bValue/(2.0*aValue)))
-
+        singleSolution = (-bValue/(2.0*aValue))
+        return singleSolution
+    
     elif (discriminant) > 0:
         discriminantText = ("Diskriminanten er\n", "The discriminant is\n",)
         print (discriminantText[LANG], discriminant)
-        result = ("Løsning 1 er:\n", "Solution 1 is:\n")
-        print (result[LANG],(-bValue + math.sqrt(discriminant))/(2.0*aValue))
-        result2 = ("Løsning 2 er:\n", "Solution 2 is:\n")
-        print (result2[LANG], (-bValue - math.sqrt(discriminant))/(2.0*aValue))
+        result = (-bValue + math.sqrt(discriminant))/(2.0*aValue)
+        resultText = ("Løsning 1 er:\n", "Solution 1 is:\n")
+        print (resultText[LANG], result)
+        result2 = (-bValue - math.sqrt(discriminant))/(2.0*aValue)
+        result2Text = ("Løsning 2 er:\n", "Solution 2 is:\n")
+        print (result2Text[LANG], result2)
 
-    return    
+        #Toppunkt
+        toppunktText = ("Toppunktet er:\n", "The summit is:\n")
+        toppunktx = -(bValue/(2.0*aValue))
+        toppunkty = -(discriminant/(4.0*aValue))
+        print (toppunktText[LANG], toppunktx, toppunkty)
+
+        return result, result2, toppunktx, toppunkty
+
+               
+
+    return     
 andengradsligning()
